1c615ce90519fe59b39cac0a5e125f6aeb172461
Matatini Reia <matatini.reia@ipilyon.net>
2018-11-30 09:47:33 +0100

    models ok

---
+++ src/app/models/vehicule.ts
@@ -0,0 +1,18 @@
+export class Vehicule {
+  name: string;
+  model: string;
+  vehicle_class: string;
+  manufactured: string;
+  length: string;
+  cost_in_credits: string;
+  crew: string;
+  passengers: string;
+  max_atmospheric_speed: string;
+  cargo_capacity: string;
+  consumables: string;
+  films: string[];
+  pilots: string[];
+  url: string;
+  created: string;
+  edited: string;
+}
