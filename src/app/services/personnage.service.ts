ce8959a2a05e0ab447e0d043c36674f6adcf04f0
shinlabs <shin_taku@hotmail.com>
2018-12-01 17:44:59 +0100

    corrected personnage service

--- src/app/services/personnage.service.ts
+++ src/app/services/personnage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Vehicule } from '../models/vehicule';
+import { Personnage } from '../models/personnage';
 import { Observable } from 'rxjs/internal/Observable';
 
 const URL_API = environment.apiUrl;
@@ -13,7 +13,7 @@ export class PersonnageService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getPersonnage(id: number): Observable<Vehicule> {
-    return this.httpClient.get<Vehicule>(URL_API + '/people/' + id);
+  getPersonnage(id: number): Observable<Personnage> {
+    return this.httpClient.get<Personnage>(URL_API + '/people/' + id);
   }
 }
