3d72a1ec16f6cc068282ea56c1032629af2c52c7
Matatini Reia <matatini.reia@ipilyon.net>
2018-12-06 09:30:53 +0100

    toolbar finished - simplified version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DisplayWholeListComponent} from './components/display-whole-list/display-whole-list.component';
 import {DisplayDetailsComponent} from './components/display-details/display-details.component';
+import {MainComponent} from './components/main/main.component';
 
 const routes: Routes = [
   {path: 'personnage/all', component: DisplayWholeListComponent},
   {path: 'vehicule/all', component: DisplayWholeListComponent},
   {path: 'personnage/details/:name', component: DisplayDetailsComponent},
-  {path: 'vehicule/details/:name', component: DisplayDetailsComponent}
+  {path: 'vehicule/details/:name', component: DisplayDetailsComponent},
+  {path: '', component: MainComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
 @NgModule({
